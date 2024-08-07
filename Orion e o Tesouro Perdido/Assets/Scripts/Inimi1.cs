3acafc4cee81708310a75e5d9901477da999cfb6
Diogo Geronimo do Nascimento Silva <20211170150035@ifrn.local>
2024-08-07 08:53:58 -0300

    script inimigo1

--- Orion e o Tesouro Perdido/Assets/Scripts/Inimi1.cs
+++ Orion e o Tesouro Perdido/Assets/Scripts/Inimi1.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Inimi1 : MonoBehaviour
 {
+    public float speed;
+    public float walkTime;
+    private float timer;
+    private bool walkRigth = true;
+    private Rigidbody2D rig;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        timer += Time.deltaTime;
+        if (timer >= walkTime)
+        {
+            walkRigth = !walkRigth;
+            timer = 0f;
+
+        }
+
+        if (walkRigth)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            rig.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            rig.velocity = Vector2.left * speed;
+        }
+        
+        
         
     }
 }
