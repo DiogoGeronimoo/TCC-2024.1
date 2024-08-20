8560aa5c104ca406794c8df265e499e2520b41ce
Diogo Geronimo do Nascimento Silva <20211170150035@ifrn.local>
2024-08-20 17:07:34 -0300

    inimigo2

--- Orion e o Tesouro Perdido/Assets/Scripts/Player.cs
+++ Orion e o Tesouro Perdido/Assets/Scripts/Player.cs
@@ -26,7 +26,7 @@ public class player : MonoBehaviour
     {
         rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        atkCollider.SetActive(false);
+        //atkCollider.SetActive(false);
         //GameControler.instance.UpdateLives(health);
         respawnPoint = transform.position;
 
