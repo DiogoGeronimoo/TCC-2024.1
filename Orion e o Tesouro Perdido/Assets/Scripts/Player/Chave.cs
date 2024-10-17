65c9bcb073d69876d636ab07a8e1a0adcbce3538
Diogo Geronimo do Nascimento Silva <20211170150035@ifrn.local>
2024-10-17 16:34:17 -0300

    subistituir

---
+++ Orion e o Tesouro Perdido/Assets/Scripts/Player/Chave.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chave : MonoBehaviour
+{
+     public int scoreValue;
+    
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                col.gameObject.GetComponent<player>().IncreaseLife(scoreValue);
+                Destroy(gameObject);
+            }
+        }
+}
