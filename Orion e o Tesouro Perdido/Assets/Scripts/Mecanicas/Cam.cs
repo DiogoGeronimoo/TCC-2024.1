4c55f029c6e71e8e353f1c796e452f9ab39b9429
Diogo Geronimo do Nascimento Silva <20211170150035@ifrn.local>
2024-10-22 11:18:11 -0300

    chao provisorio/ camera distancia

--- Orion e o Tesouro Perdido/Assets/Scripts/Mecanicas/Cam.cs
+++ Orion e o Tesouro Perdido/Assets/Scripts/Mecanicas/Cam.cs
@@ -16,7 +16,7 @@ public class Cam : MonoBehaviour
     // Atualiza alguma coisa da camera
     void Update()
     {
-        if(player.position.x >= -1.45 && player.position.x <= 160 )
+        if(player.position.x >= -1.45 && player.position.x <= 272.8 )
         {
             Vector3 following = new Vector3(player.position.x, transform.position.y, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime);
